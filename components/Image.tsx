b409eda3fe6492b258de224a44b98299ec5d2a42
no149 <mehrmor@live.com>
2023-05-20 21:33:02 +0330

    Adding image picker to note form.

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
-import { Image, ImageStyle, StyleProp, StyleSheet } from 'react-native'
-import Base64ArrayBuffer from 'base64-arraybuffer'
+import { GestureResponderEvent, Image, ImageStyle, StyleProp, StyleSheet, TouchableOpacity } from 'react-native'
 import { ImageContent } from '../model/note'
 import React from 'react'
+import * as ImagePicker from 'expo-image-picker';
+import { isPropertyAccessOrQualifiedName } from 'typescript';
 
 export default ({
   image,
@@ -17,5 +18,22 @@ export default ({
   style: StyleProp<ImageStyle>
   editable: boolean
 }) => {
-  return <Image source={image.content} resizeMode="cover" style={style} />
+  const [imageSource,setImageSource] = useState({content:image.content,width,height})
+   console.log('image source',imageSource)
+  const imageComponent =  <Image source={imageSource.content} resizeMode="cover" style={[{minWidth:imageSource.width,minHeight:imageSource.height},style]} />
+  async function showImagePicker() {
+  
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: false,
+      quality: 1,
+      allowsMultipleSelection:false
+    });
+    if (!result.canceled) {
+      let asset = result.assets[0];
+setImageSource({content:{uri:result.assets[0].uri},height:asset.height,width:asset.width})
+    } else {
+      alert('You did not select any image.');
+    }  }
+
+  return (editable? <TouchableOpacity onPress={showImagePicker}>{imageComponent}</TouchableOpacity>:imageComponent)
 }
